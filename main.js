e1c65d39c020a6205e414a288a9f156a970dd469
sanzhikee <sanzhar@thefroot.com>
2019-08-04 17:52:56 +0600

    add updates

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const {app, BrowserWindow} = require('electron');
+const {autoUpdater} = require("electron-updater");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
+function sendStatusToWindow(text) {
+    win.webContents.send('message', text);
+}
+
 function createWindow() {
+    autoUpdater.checkForUpdatesAndNotify();
+
     // Create the browser window.
     win = new BrowserWindow({
         fullscreen: true,
@@ -28,6 +35,28 @@ function createWindow() {
         win = null
     });
 }
+
+autoUpdater.on('checking-for-update', () => {
+    sendStatusToWindow('Checking for update...');
+});
+autoUpdater.on('update-available', (info) => {
+    sendStatusToWindow('Update available.');
+});
+autoUpdater.on('update-not-available', (info) => {
+    sendStatusToWindow('Update not available.');
+});
+autoUpdater.on('error', (err) => {
+    sendStatusToWindow('Error in auto-updater. ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+});
+autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update downloaded');
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
